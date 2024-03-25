20b2a2c6366dc59b421699230c39dc1bc17968cd
TriciaBAA <pat.aspillaga@gmail.com>
2024-03-25 21:20:55 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var button = document.querySelector('button');
+    button.addEventListener('click', function () {
+        document.body.style.backgroundColor = '#DDFFEA';
+    })
+})
